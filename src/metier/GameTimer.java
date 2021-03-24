adb51e5ffed3bd8df45b8536b537f584c9a8498c
ToihirHalim <66559721+ToihirHalim@users.noreply.github.com>
2021-03-24 11:59:09 +0100

    add GameTimer class

---
+++ src/metier/GameTimer.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metier;
+
+/**
+ *
+ * @author Toihir
+ */
+public class GameTimer {
+    public int min;
+    public int sec;
+    
+    public GameTimer(int min, int sec){
+        this.min = min;
+        this.sec = sec;
+    }
+    
+    public boolean removeSec(){
+        if(this.sec > 0){
+            this.sec--;
+            return true;
+        }else if(min > 0) {
+            min--;
+            this.sec = 59;
+            return true;
+        }
+        this.min = 0;
+        this.sec = 0;
+        return false;
+    }
+    
+    public String toString(){
+        String minString = this.min >= 10 ? "" + this.min : "0" + this.min;
+        String secString = this.sec >= 10 ? "" + this.sec : "0" + this.sec;
+        return minString + " : " + secString;
+    }
+}
